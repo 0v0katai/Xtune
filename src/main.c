962c3435eb9ce3e71ff959105ac02516332a67e5
CalcLoverHK <0v0katai@gmail.com>
2026-04-06 13:13:19 +0800

    feat: global config file save

--- src/main.c
+++ src/main.c
@@ -56,20 +56,27 @@ static bool global_getkey(key_event_t key)
     #ifdef ENABLE_HELP
     if (key.key == KEY_OPEN_HELP && !help_status)
         call_help_function();
-    if (shift && key.key == KEY_ACON)
-    {
-        # if defined CG50 && !defined TARGET_FXCG50_FASTLOAD
-        info_box(5, 1, C_BLACK, "Caution",
-            "Poweroff function is disabled in this build.\n"
-            "Please return to the main menu before\n"
-            "turning off your calculator.");
-        xtune_getkey();
-        # else
-        gint_poweroff(true);
-        # endif
-        shift = false;
-    }
     #endif
+    if (shift) {
+        if (key.key == KEY_ACON) {
+            # if defined CG50 && !defined TARGET_FXCG50_FASTLOAD
+            info_box(5, 1, C_BLACK, "Caution",
+                "Poweroff function is disabled in this build.\n"
+                "Please return to the main menu before\n"
+                "turning off your calculator.");
+            xtune_getkey();
+            # else
+            gint_poweroff(true);
+            # endif
+            shift = false;
+        }
+        if (key.key == KEY_SAVE_CONFIG && !save_config()) {
+            info_box(5, 1, C_RED, "ERROR",
+                "Failed to write config to file!");
+            xtune_getkey();
+            shift = false;
+        }
+    }
     return false;
 }
 
